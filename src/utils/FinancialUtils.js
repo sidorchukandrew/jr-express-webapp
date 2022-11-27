705d19d95ea01e963f9112be6a0a9d5ef35ee41b
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2022-11-27 11:22:16 -0500

    Formatting the currency better

--- src/utils/FinancialUtils.js
+++ src/utils/FinancialUtils.js
@@ -1,24 +1,27 @@
 export function calculateTotal(form) {
-	let lumper = "lumper" in form ? Number.parseFloat(form.lumper) : 0;
-	let loadPay = "load_pay" in form ? Number.parseFloat(form.load_pay) : 0;
+  let lumper = 'lumper' in form ? Number.parseFloat(form.lumper) : 0;
+  let loadPay = 'load_pay' in form ? Number.parseFloat(form.load_pay) : 0;
 
-	let total = lumper + loadPay;
+  let total = lumper + loadPay;
 
-	return total.toFixed(2);
+  return total.toFixed(2);
 }
 
 export function calculateGrandTotal(invoices = []) {
-	let grandTotal = 0;
+  let grandTotal = 0;
 
-	invoices.forEach((invoice) => {
-		if ("lumper" in invoice) {
-			grandTotal += Number.parseFloat(invoice.lumper);
-		}
+  invoices.forEach(invoice => {
+    if ('lumper' in invoice) {
+      grandTotal += Number.parseFloat(invoice.lumper);
+    }
 
-		if ("load_pay" in invoice) {
-			grandTotal += Number.parseFloat(invoice.load_pay);
-		}
-	});
+    if ('load_pay' in invoice) {
+      grandTotal += Number.parseFloat(invoice.load_pay);
+    }
+  });
 
-	return grandTotal.toFixed(2);
+  return grandTotal.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
 }
